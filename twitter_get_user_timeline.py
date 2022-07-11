5fa3308e4a691063c448d4ff6fd066ff71958f3e
Neetika Tripathi <64384522+nitri166@users.noreply.github.com>
2022-07-11 18:23:09 +0530

    Add files via upload

---
+++ twitter_get_user_timeline.py
@@ -0,0 +1,14 @@
+import sys
+import json
+from tweepy import Cursor
+from twitter_client import get_twitter_client
+
+if __name__== '__main__':
+    user = sys.argv[1]
+    client = get_twitter_client()
+
+    fname= "user_timeline_{}.jsonl".format(user)
+    with open(fname,'w') as f:
+        for page in Cursor(client.user_timeline, screen_name=user, count=200).pages(16):
+            for status in page:
+                f.write(json.dumps(status._json)+"\n")
